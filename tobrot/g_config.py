f2d5fbcdece8696eef3c7b1e0384e9eb6b39e468
mrmythpat <75902632+mrmythpat@users.noreply.github.com>
2020-12-13 12:00:21 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -8,6 +8,6 @@ class Config(Config):
     OWNER_ID = 12537936
     AUTH_CHANNEL = [-10082786282972]
     DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCKWJVlEr2sfXpIQmiv1wv3vsIyAT9AszwrwqCqpJKvKpeoy3lZ1R2AGUH3SCrB5kxbxUxSbm_AnABtCXhUyOF7mshFiy5IQGjnIJadgbAeIodkuLuP3uzqVOosZ22HUfQmiMXM_tzptq_65LhEr8_reVmBB0HjGHvyCF4","refresh_token":"1//0dYiWpOwvPC5lCgYIARAAGA0SNwF-L9IrWBSpI-XCR3Kk-brXh95zwOr8IiQO6SyZY3ypZQ0ekXPz7RhQfWst__EqIh8pid8XvRE","scope":"https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.metadata.readonly","token_type":"Bearer","expiry_date":1607798137208}
     #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
     #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
